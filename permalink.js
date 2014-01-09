6bdcb75018bfc05d775c8570c438e3bf1da07c90
Choi Goon-ho <caiguanhao@gmail.com>
2014-01-10 02:57:53 +0800

    add multiple-permalink option; fix bug; get permalink in any page; now you can use an permalink array or a multiline permalink string to generate more than one page from the same template; also you can read permalink variable in all pages; permalink is the source path if the page does not have a permalink;

--- permalink.js
+++ permalink.js
@@ -10,40 +10,82 @@ module.exports = function(params, callback) {
   var options  = assemble.options;
   var pages    = options.pages;
   var defaults = options.permalink;
+  var parseTpl = function(input, data, page) {
+    try {
+      return _.template(input, data);
+    } catch (error) {
+      grunt.fail.fatal(error.message + ' at ' + page.src);
+    }
+  };
 
   // convert {{ variable }} to <%= variable %>
   if (_.isString(defaults) && !_.isEmpty(defaults)) {
     defaults = defaults.replace(/{{/g, '<%=').replace(/}}/g, '%>');
   }
 
-  async.forEach(pages, function(page, next) {
+  async.each(pages, function(page, next) {
+
+    page.data = page.data || {};
 
     var yfm = page.data;
     var permalink = defaults;
 
-    if (!_.isUndefined(yfm) && !_.isUndefined(yfm.permalink)) {
+    if (!_.isUndefined(yfm.permalink)) {
       permalink = yfm.permalink;
     }
 
-    if (_.isString(permalink) && !_.isEmpty(permalink)) {
+    if (!_.isEmpty(permalink)) {
+
       var data = _.assign({}, options, yfm);
+      var permalinks;
 
-      try {
-        permalink = _.template(permalink, data);
-      } catch (error) {
-        grunt.log.error(error);
+      if (_.isString(permalink)) {
+
+        permalink = parseTpl(permalink, data, page);
+        permalink = permalink.trim();
+        permalinks = permalink.split('\n');
+
+      } else if (_.isArray(permalink)) {
+
+        permalinks = permalink;
+
+      } else {
+        grunt.fail.fatal('the permalink of ' + page.src +
+          ' is neither a string nor an array');
       }
 
-      var dirname = page.dirname;
-      if (page.filePair && page.filePair.dest) {
-        dirname = page.filePair.dest;
-      }
+      for (var i = permalinks.length - 1; i >= 0; i--) {
+        var permalink = permalinks[i];
+        if (_.isEmpty(permalink)) continue;
 
-      page.dest = path.join(dirname, permalink);
+        permalink = parseTpl(permalink, data, page);
+        permalink = permalink.trim();
+        if (_.isEmpty(permalink)) continue;
 
-      if (page.dest.slice(-1) === path.sep.slice(-1)) {
-        page.dest = path.join(page.dest, 'index.html');
+        var dirname = page.dirname;
+        if (page.filePair && page.filePair.dest) {
+          dirname = page.filePair.dest;
+        }
+
+        page.dest = path.join(dirname, permalink);
+
+        if (page.dest.slice(-1) === path.sep.slice(-1)) {
+          page.dest = path.join(page.dest, 'index.html');
+        }
+
+        if (page.data) {
+          page.data.permalink = permalink;
+        }
+
+        if (i > 0) {
+          page = _.cloneDeep(page);
+          pages.push(page);
+        }
       }
+    }
+
+    if (!page.data.permalink) {
+      page.data.permalink = page.src;
     }
 
     next();
